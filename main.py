15d61900fbd519832c10aa3af5145d899a7e942c
grissomlin <grissomlin643@gmail.com>
2026-01-03 14:01:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv() 
 
 socket.setdefaulttimeout(600)
-GDRIVE_FOLDER_ID = '1ltKCQ209k9MFuWV6FIxQ1coinV2fxSyl' 
+GDRIVE_FOLDER_ID = os.environ.get('GDRIVE_FOLDER_ID')
 SERVICE_ACCOUNT_FILE = 'citric-biplane-319514-75fead53b0f5.json'
 
 try:
@@ -180,3 +180,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
